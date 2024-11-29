13f4560ee11f5dc1f1d560ce0fea240d94d41612
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-11-30 01:03:34 +0530

    add: font and color

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 function About() {
   return (
     <div>
-      <div className="text-blue-600 text-6xl flex justify-center p-10">About Us</div>
-      <div className="p-16 pt-10 font-serif flex justify-center pb-16 text-center">
+      <div className="text-[#0064E0] text-6xl flex justify-center p-10">About Us</div>
+      <div className="p-16 pt-10 font-[poppins] text-xl flex justify-center pb-16 text-center">
         Entrepreneurship Cell, NIT Rourkela is an endeavor of the institute through the student body to encourage "Innovation, Incubation and Entrepreneurship." It was founded in 2007 and operates under Technical Society, SAC. Its main objective is to promote and support entrepreneurship by developing entrepreneurial traits among the students and helping budding entrepreneurs achieve their goals. To encourage entrepreneurship, ECell at NITR organizes various events and supports budding talents in the race for finding their dream venture. The major initiatives taken by ECell are National Entrepreneurship Summit, Arthayan and Weekend activities. ECell also organizes entrepreneurship based activities during weekends throughout the year. Such activities have two main motives: promote the entrepreneurial culture among students and support the already confident students in their run towards entrepreneurship.
       </div>
       
@@ -28,7 +28,7 @@ function About() {
               className="mx-auto"
             />
           </div>
-          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">STUDENTS</span>
+          <span className="block text-[#0064E0] sm:text-4xl text-xl md:text-4xl mt-4">STUDENTS</span>
         </div>
         <div className="p-4 flex flex-col justify-between h-full">
           <div className="flex flex-col items-center gap-4">
@@ -47,7 +47,7 @@ function About() {
               className="mx-auto"
             />
           </div>
-          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">SPEAKERS</span>
+          <span className="block text-[#0064E0] sm:text-4xl text-xl md:text-4xl mt-4">SPEAKERS</span>
         </div>
         <div className="p-4 flex flex-col justify-between h-full">
           <div className="flex flex-col items-center gap-4">
@@ -66,7 +66,7 @@ function About() {
               className="mx-auto"
             />
           </div>
-          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">STARTUPS</span>
+          <span className="block text-[#0064E0] sm:text-4xl text-xl md:text-4xl mt-4">STARTUPS</span>
         </div>
         <div className="p-4 flex flex-col justify-between h-full">
           <div className="flex flex-col items-center gap-4">
@@ -85,7 +85,7 @@ function About() {
               className="mx-auto"
             />
           </div>
-          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">INVESTORS</span>
+          <span className="block text-[#0064E0] sm:text-4xl text-xl md:text-4xl mt-4">INVESTORS</span>
         </div>
       </div>
     </div>
