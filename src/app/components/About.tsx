e39c10c028d3eac34d601881ab8b6f1f1e9d07c3
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-11-30 00:19:38 +0530

    about us designed

---
+++ src/app/components/About.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+function About() {
+  return (
+    <div>
+      <div className="text-blue-600 text-6xl flex justify-center p-10">About Us</div>
+      <div className="p-16 pt-10 font-serif flex justify-center pb-16 text-center">
+        Entrepreneurship Cell, NIT Rourkela is an endeavor of the institute through the student body to encourage "Innovation, Incubation and Entrepreneurship." It was founded in 2007 and operates under Technical Society, SAC. Its main objective is to promote and support entrepreneurship by developing entrepreneurial traits among the students and helping budding entrepreneurs achieve their goals. To encourage entrepreneurship, ECell at NITR organizes various events and supports budding talents in the race for finding their dream venture. The major initiatives taken by ECell are National Entrepreneurship Summit, Arthayan and Weekend activities. ECell also organizes entrepreneurship based activities during weekends throughout the year. Such activities have two main motives: promote the entrepreneurial culture among students and support the already confident students in their run towards entrepreneurship.
+      </div>
+      
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 m-10 text-center">
+        <div className="p-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col items-center gap-4">
+            <Image
+              src="/Layer_2.png"
+              alt="students"
+              width={200}
+              height={200}
+              className="mx-auto"
+            />
+            <Image
+              src="/1K+.png"
+              alt="students"
+              width={100}
+              height={100}
+              className="mx-auto"
+            />
+          </div>
+          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">STUDENTS</span>
+        </div>
+        <div className="p-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col items-center gap-4">
+            <Image
+              src="/Layer_1.png"
+              alt="entrepreneurship"
+              width={200}
+              height={200}
+              className="mx-auto"
+            />
+            <Image
+              src="/20+.png"
+              alt="students"
+              width={100}
+              height={100}
+              className="mx-auto"
+            />
+          </div>
+          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">SPEAKERS</span>
+        </div>
+        <div className="p-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col items-center gap-4">
+            <Image
+              src="/Layer_1 (1).png"
+              alt="entrepreneurship"
+              width={200}
+              height={200}
+              className="mx-auto"
+            />
+            <Image
+              src="/50+.png"
+              alt="students"
+              width={100}
+              height={100}
+              className="mx-auto"
+            />
+          </div>
+          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">STARTUPS</span>
+        </div>
+        <div className="p-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col items-center gap-4">
+            <Image
+              src="/Layer_1 (2).png"
+              alt="entrepreneurship"
+              width={200}
+              height={200}
+              className="mx-auto"
+            />
+            <Image
+              src="/100+.png"
+              alt="students"
+              width={100}
+              height={100}
+              className="mx-auto"
+            />
+          </div>
+          <span className="block text-blue-600 sm:text-4xl text-xl md:text-4xl mt-4">INVESTORS</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/app/components/Event.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function Event() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Event
