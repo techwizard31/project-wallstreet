83f0fa868d190d84883c55f5c72173da33e900d7
narashimha05 <chinnarinarashimha@gmail.com>
2024-12-03 00:15:37 +0530

    feat: Add Sponsors and Team and Font Poppins

---
+++ src/Data/SponsorData.js
@@ -0,0 +1,95 @@
+export const sponsorData = [{
+    id: 1,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161244/sponsorDataE-cell/fzxjxpj1kxfwmqzaf9rj.png",
+    alt: "Frame 54"
+},
+{
+    id: 2,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161545/sponsorDataE-cell/fsjxtvdw2k7fe9qfowe2.png",
+    alt: "Frame 55"
+},
+{
+    id: 3,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161739/sponsorDataE-cell/ovnl7oo75kh8n3zmxiwm.png",
+    alt: "Frame 56"
+},
+{
+    id: 4,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161740/sponsorDataE-cell/dujxiagbppshtca7ugxq.png",
+    alt: "Frame 57"
+},
+{
+    id: 5,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161740/sponsorDataE-cell/zc6hqluebmvzfllko06s.png",
+    alt: "Frame 58"
+},
+{
+    id: 6,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161740/sponsorDataE-cell/gtm3ghiaa021wbmhvz4k.png",
+    alt: "Frame 59"
+},
+{
+    id: 7,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733161740/sponsorDataE-cell/aqldylaspgplsl4yvgcw.png",
+    alt: "Frame 60"
+},
+{
+    id: 8,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162056/sponsorDataE-cell/arci2zz99ppotmwmyymt.png",
+    alt: "Frame 61"
+},{
+    id: 9,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162056/sponsorDataE-cell/habujfjqseuetixvnu6m.png",
+    alt: "Frame 62"
+},{
+    id: 10,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162057/sponsorDataE-cell/jmbhdakz1ta1iyszvxlp.png",
+    alt: "Frame 63"
+},{
+    id: 11,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162057/sponsorDataE-cell/jmdxffhebe4vqoabykfn.png",
+    alt: "Frame 64"
+},{
+    id: 12,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162057/sponsorDataE-cell/ce9hihvrbrcucdxgzavz.png",
+    alt: "Frame 65"
+},
+{
+    id: 13,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162261/sponsorDataE-cell/miybwycdkmv17ctcmagq.png",
+    alt: "Frame 66"
+},{
+    id: 14,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162261/sponsorDataE-cell/wtl51czkxflzhnvw2htr.png",
+    alt: "Frame 67"
+},{
+    id: 15,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162261/sponsorDataE-cell/tysqupwr6qqzvppkct9l.png",
+    alt: "Frame 68"
+},{
+    id: 16,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162261/sponsorDataE-cell/ojq0ghy7lj2tmdutz9zk.png",
+    alt: "Frame 69"
+},{
+    id: 17,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162261/sponsorDataE-cell/srfworzkkfhila8hqbmq.png",
+    alt: "Frame 70"
+},
+{
+    id: 18,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162432/sponsorDataE-cell/myg8d38fvbuag5rs7fzj.png",
+    alt: "Frame 71"
+},{
+    id: 19,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162433/sponsorDataE-cell/omfnfxqo5xw7xfesf5pw.png",
+    alt: "Frame 72"
+},{
+    id: 20,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162432/sponsorDataE-cell/bnavd9zfwvtngz3rzjzq.png",
+    alt: "Frame 73"
+},{
+    id: 21,
+    imageUrl: "https://res.cloudinary.com/dlp0vynmo/image/upload/v1733162433/sponsorDataE-cell/f5nbkyqtlshkx5dzc4t8.png",
+    alt: "Frame 74"
+}
+]
