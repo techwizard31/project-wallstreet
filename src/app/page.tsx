0096f102d094b57420062c2f4af569e293297b76
mdsibtainhabib <sibtainhabib413@gmail.com>
2024-12-02 11:39:39 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+"use client"
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import About from "../components/About";
 import Event from "../components/Event";
 import Loading from "./loading";
-import { Suspense } from "react";
 import Hero from "@/components/Hero";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    // Cleanup the timer
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <Suspense fallback={<Loading/>}>
-        <Hero/>
-        <About/>
-        <Event/>
-      </Suspense>
+      <Hero/>
+      <About />
+      <Event />
     </>
   );
 }
