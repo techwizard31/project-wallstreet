9f0db9aad1f3823d6a9e63f3a82a0547d3d7d126
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-12-03 19:45:18 +0530

    loading time fixed

--- src/components/loadingComp.tsx
+++ src/components/loadingComp.tsx
@@ -11,7 +11,7 @@ export default function Loading() {
   useEffect(() => {
     const interval = setInterval(() => {
       setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 800);
+    }, 300);
     return () => clearInterval(interval); 
   }, []);
 
